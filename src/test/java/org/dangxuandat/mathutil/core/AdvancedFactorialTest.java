4d5ca5b18d2d5cfbb8f919705579fd54ce5cc2b6
Dat <dangxuandat0@gmail.com>
2021-09-29 14:00:28 +0700

    test CI CD second time

--- src/test/java/org/dangxuandat/mathutil/core/AdvancedFactorialTest.java
+++ src/test/java/org/dangxuandat/mathutil/core/AdvancedFactorialTest.java
@@ -49,7 +49,7 @@ public class AdvancedFactorialTest {
                         {3, 6},
                         {4, 24},
                         {5, 120},
-                        {6, 820},
+                        {6, 720},
                 });
                  //bổ sung test CI
                 //cố tình màu đỏ
